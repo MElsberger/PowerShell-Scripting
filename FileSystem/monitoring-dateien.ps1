722c3ff422c534a785b261c5fd94329a082ff886
MElsberger <72889279+MElsberger@users.noreply.github.com>
2021-11-08 22:58:23 +0100

    Create monitoring-dateien.ps1

---
+++ FileSystem/monitoring-dateien.ps1
@@ -0,0 +1,17 @@
+# Das Verzeichnis muss natürlich existieren
+# Über die Endlose While Schleife wird geprüft, ob es Änderungen am Verzeichnis gab.
+$folder = ’l:Projekte’
+$timeout = 10
+
+$FileSystemWatcher = New-Object System.IO.FileSystemWatcher $folder
+Write-Host ”Press CTRL+C to abort monitoring $folder”
+while ($true) {
+$result = $FileSystemWatcher.WaitForChanged(‘all’, $timeout)
+if ($result.TimedOut -eq $false)
+{
+
+Write-Warning (‘File {0} : {1}’ -f $result.ChangeType, $result.name)
+
+}
+}
+Write-Host ’Monitoring aborted.’
