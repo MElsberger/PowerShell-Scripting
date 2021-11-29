26e3e658ef515f35ab1e8048a60cd2bb6e5600bd
MElsberger <72889279+MElsberger@users.noreply.github.com>
2021-11-29 14:16:11 +0100

    Create parameteruebergabe.ps1

---
+++ Random/parameteruebergabe.ps1
@@ -0,0 +1,11 @@
+
+# Im Deklarationsteil werden die Parameter zugewiesen
+param (
+
+$variable1 = "Hallo ",
+$variable2 = "Markus "
+)
+
+
+# So können die einzelnen Parameter ausgegeben werden
+Write-Host $variable1,$variable2
