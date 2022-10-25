21ec6dd62d46bd7d41129968e23c1f7488315b2e
MElsberger <72889279+MElsberger@users.noreply.github.com>
2022-10-25 16:39:16 +0200

    Add files via upload

---
+++ FileSystem/Bsp-Skripting.ps1
@@ -0,0 +1,36 @@
+﻿<#
+.SYNOPSIS
+Auslesen der Dateigrößen
+
+.DESCRIPTION
+Mit diesem Script kann in einem bestimmten Verzeichnis 
+die Größe der Dateien ausgelesen werden
+
+.PARAMETER
+$location für die Pfad, 
+$length für die Dateigröße
+$subdir für das auslesen der Unterverzeichnisse
+
+.EXAMLE
+Get-LargeFiles -location d:\ -length 10 -subdir
+
+.NOTES
+Falls subdir aktiv werden die Unterverzeichnisse ebenfalls ausgelesen
+
+.LINK
+Markus Elsberger 
+IT-Learner.de
+
+#>
+
+function Get-LargeFiles ($location = "k:\", $length=100, [switch]$subdir)
+{
+    $schalter=$false
+        IF($subdir)
+        {
+        $schalter=$True
+    }
+        Get-ChildItem $location -Recurse:$schalter | Where-Object {$_.Length -ge $length}
+    }
+
+Get-LargeFiles -location d:\ -length 10 -subdir
